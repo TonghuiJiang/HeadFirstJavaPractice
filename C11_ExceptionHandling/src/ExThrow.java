e05263044a5b1e5d6b0388635ac6e70388945707
TonghuiJiang <jiangt14@mcmaster.ca>
2018-10-09 12:54:36 -0400

    Try-Catch block implementation

---
+++ C11_ExceptionHandling/src/ExThrow.java
@@ -0,0 +1,33 @@
+public class ExThrow {
+//    public void takeRisk() throws BadException {
+//        if (abandonAllHope) {
+//            throw new BadException();
+//        }
+//    }
+//
+//    public void crossFingers() {
+//        try {
+//            anObject.takeRisk();
+//        } catch (BadException ex) {
+//            System.out.println();("Aaargh!");
+//            ex.printStackTrace();
+//        }
+//    }
+        public static int takeRisk() throws Exception {
+        if (1+1==2) {
+            throw new Exception();
+        }
+        else
+            return 1;
+    }
+
+    public static void main(String[] args) {
+        try {
+            int i = takeRisk();
+            System.out.println(i);
+        } catch (Exception ex) {
+            System.out.println("Failed");
+        }
+        System.out.println("We made it!");
+    }
+}
